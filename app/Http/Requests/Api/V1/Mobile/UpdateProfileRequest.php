4d5f3d6a4a426fbe9bb0532266aac04a59ba2d51
iztileuoff <bektemir0714@gmail.com>
2024-12-24 12:35:00 +0500

    add profile

--- app/Http/Requests/Api/V1/Mobile/UpdateProfileRequest.php
+++ app/Http/Requests/Api/V1/Mobile/UpdateProfileRequest.php
@@ -3,12 +3,14 @@
 namespace App\Http\Requests\Api\V1\Mobile;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateProfileRequest extends FormRequest
 {
     public function rules(): array
     {
         return [
+            'phone' => ['string', 'regex:/^998([0-9][012345789]|[0-9][125679]|7[01234569])[0-9]{7}$/', Rule::unique('users', 'phone')->ignore($this->user())],
             'name' => ['string', 'max:255'],
         ];
     }
