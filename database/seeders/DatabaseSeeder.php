87da6c1fbc348d77ca5a65061a03b7d88c07f09b
iztileuoff <bektemir0714@gmail.com>
2025-01-10 11:50:52 +0500

    refactor code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,6 @@ class DatabaseSeeder extends Seeder
             LessonSeeder::class,
             OrderSeeder::class,
             MessageSeeder::class,
-            \Goodoneuz\PayUz\Database\Seeds\PayUzSeeder::class,
         ]);
     }
 }
